a60381a7cda224f0e096e749140215e4a0eb38f4
StoppedwummSites <150438484+StoppedwummSites@users.noreply.github.com>
2024-11-04 10:47:23 +0100

    add error handling

--- src/publicSrc/ably.js
+++ src/publicSrc/ably.js
@@ -46,13 +46,17 @@ async function ablyMain() {
   } else {
     try {
       await window.PokiSDK.init()
+      await window.PokiSDK.gameLoadingFinished()
+      await window.PokiSDK.gameplayStarted()
     } catch (e) {
       console.log(e, "but fuck it")
     }
-    await window.PokiSDK.gameLoadingFinished()
-    await window.PokiSDK.gameplayStarted()
     setInterval(async () => {
-      await window.PokiSDK.commercialBreak()
+      try {
+        await window.PokiSDK.commercialBreak()
+      } catch (e) {
+        console.log(e, "ad error")
+      }
     }, 30000)
   }
   if (localStorage.getItem("joinCode") == undefined || localStorage.getItem("username") == undefined) {
